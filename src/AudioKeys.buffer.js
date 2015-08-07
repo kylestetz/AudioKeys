27ef94768903a47f825b3a9cf4c1c7c556a80d25
Kyle Stetz <kyle@punkave.com>
2015-08-07 07:15:39 -0400

    updated _addKey and _removeKey ... they work!

--- src/AudioKeys.buffer.js
+++ src/AudioKeys.buffer.js
@@ -1,3 +1,7 @@
+// ================================================================
+// KEY BUFFER
+// ================================================================
+
 // The process is:
 
 // key press
@@ -14,13 +18,15 @@ AudioKeys.prototype._addKey = function(e) {
   if(self._isNote(e.keyCode) && !self._isPressed(e.keyCode)) {
     var newKey = {
       note: self._map(e.keyCode),
-      keyCode: e.keyCode
+      keyCode: e.keyCode,
+      // this shouldn't be here
+      isActive: true
     };
     // add the newKey to the list of keys
     self._keys = (self._keys || []).concat(newKey);
     // reevaluate the active notes based on our priority rules.
     // give it the new note to use if there is an event to trigger.
-    self._prioritize(newKey);
+    // self._prioritize(newKey);
   }
 };
 
@@ -31,17 +37,43 @@ AudioKeys.prototype._removeKey = function(e) {
     var keyToRemove;
     for(var i = 0; i < self._keys.length; i++) {
       if(self._keys[i].keyCode === e.keyCode) {
-        keyToRemove = self.keys[i];
+        keyToRemove = self._keys[i];
+        break;
       }
     }
 
     if(keyToRemove.isActive) {
       // trigger an event to remove the key
-
+      self._keys.splice(self._keys.indexOf(keyToRemove), 1);
+      self._trigger('up', self._makeNote(keyToRemove));
     }
   }
 };
 
-AudioKeys.prototype._isPressed = function(e) {
+AudioKeys.prototype._isPressed = function(keyCode) {
+  var self = this;
 
+  if(!self._keys || !self._keys.length) {
+    return false;
+  }
+
+  for(var i = 0; i < self._keys.length; i++) {
+    if(self._keys[i].keyCode === keyCode) {
+      return true;
+    }
+  }
+  return false;
 };
+
+// turn a key object into a note object for the event listeners.
+AudioKeys.prototype._makeNote = function(key) {
+  return {
+    key: key.keyCode,
+    note: key.note,
+    frequency: self._toFrequency(key.note)
+  };
+};
+
+// ================================================================
+// NOTE BUFFER
+// ================================================================
