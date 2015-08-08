c422212e06db0f744a57bf0570ac6f9bab13bd9d
Kyle Stetz <kyle@punkave.com>
2015-08-08 11:17:10 -0400

    added `_update` and `_diff` methods to buffer

--- src/AudioKeys.buffer.js
+++ src/AudioKeys.buffer.js
@@ -5,7 +5,7 @@
 // The process is:
 
 // key press
-//   add to self._keys
+//   add to self._state.keys
 //   (an accurate representation of keys currently pressed)
 // resolve self.buffer
 //   based on polyphony and priority, determine the notes
@@ -16,17 +16,12 @@ AudioKeys.prototype._addKey = function(e) {
   // if the keyCode is one that can be mapped and isn't
   // already pressed, add it to the key object.
   if(self._isNote(e.keyCode) && !self._isPressed(e.keyCode)) {
-    var newKey = {
-      note: self._map(e.keyCode),
-      keyCode: e.keyCode,
-      // this shouldn't be here
-      isActive: true
-    };
+    var newKey = self._makeNote(e.keyCode);
     // add the newKey to the list of keys
-    self._keys = (self._keys || []).concat(newKey);
+    self._state.keys = (self._state.keys || []).concat(newKey);
     // reevaluate the active notes based on our priority rules.
     // give it the new note to use if there is an event to trigger.
-    // self._prioritize(newKey);
+    self._update();
   }
 };
 
@@ -35,17 +30,17 @@ AudioKeys.prototype._removeKey = function(e) {
   // if the keyCode is active, remove it from the key object.
   if(self._isPressed(e.keyCode)) {
     var keyToRemove;
-    for(var i = 0; i < self._keys.length; i++) {
-      if(self._keys[i].keyCode === e.keyCode) {
-        keyToRemove = self._keys[i];
+    for(var i = 0; i < self._state.keys.length; i++) {
+      if(self._state.keys[i].keyCode === e.keyCode) {
+        keyToRemove = self._state.keys[i];
         break;
       }
     }
 
     if(keyToRemove.isActive) {
-      // trigger an event to remove the key
-      self._keys.splice(self._keys.indexOf(keyToRemove), 1);
-      self._trigger('up', self._makeNote(keyToRemove));
+      // remove the key from _keys
+      self._state.keys.splice(self._state.keys.indexOf(keyToRemove), 1);
+      self._update();
     }
   }
 };
@@ -53,12 +48,12 @@ AudioKeys.prototype._removeKey = function(e) {
 AudioKeys.prototype._isPressed = function(keyCode) {
   var self = this;
 
-  if(!self._keys || !self._keys.length) {
+  if(!self._state.keys || !self._state.keys.length) {
     return false;
   }
 
-  for(var i = 0; i < self._keys.length; i++) {
-    if(self._keys[i].keyCode === keyCode) {
+  for(var i = 0; i < self._state.keys.length; i++) {
+    if(self._state.keys[i].keyCode === keyCode) {
       return true;
     }
   }
@@ -66,14 +61,73 @@ AudioKeys.prototype._isPressed = function(keyCode) {
 };
 
 // turn a key object into a note object for the event listeners.
-AudioKeys.prototype._makeNote = function(key) {
+AudioKeys.prototype._makeNote = function(keyCode) {
+  var self = this;
   return {
-    key: key.keyCode,
-    note: key.note,
-    frequency: self._toFrequency(key.note)
+    keyCode: keyCode,
+    note: self._map(keyCode),
+    frequency: self._toFrequency( self._map(keyCode) )
   };
 };
 
 // ================================================================
 // NOTE BUFFER
 // ================================================================
+
+// every time a change is made to _keys due to a key on or key off
+// we need to call `_update`. It compares the `_keys` array to the
+// `buffer` array, which is the array of notes that are really
+// being played, makes the necessary changes to `buffer` and
+// triggers any events that need triggering.
+
+AudioKeys.prototype._update = function() {
+  var self = this;
+
+  // a key has been added to self._state.keys.
+  // stash the old buffer
+  var oldBuffer = self._state.buffer;
+  // set the new priority in self.state._keys
+  self._prioritize();
+  // compare the buffers and trigger events based on
+  // the differences.
+  self._diff(oldBuffer);
+};
+
+AudioKeys.prototype._diff = function(oldBuffer) {
+  var self = this;
+
+  // if it's not in the OLD buffer, it's a note ON.
+  // if it's not in the NEW buffer, it's a note OFF.
+
+  var oldNotes = oldBuffer.map( function(key) {
+    return key.keyCode;
+  });
+
+  var newNotes = self._state.buffer.map( function(key) {
+    return key.keyCode;
+  });
+
+  // check for old (removed) notes
+  var notesToRemove = [];
+  oldNotes.forEach( function(key) {
+    if(newNotes.indexOf(key) === -1) {
+      notesToRemove.push(key);
+    }
+  });
+
+  // check for new notes
+  var notesToAdd = [];
+  newNotes.forEach( function(key) {
+    if(oldNotes.indexOf(key) === -1) {
+      notesToAdd.push(key);
+    }
+  });
+
+  notesToAdd.forEach( function(key) {
+    self._trigger('down', self._makeNote(key));
+  });
+
+  notesToRemove.forEach( function(key) {
+    self._trigger('up', self._makeNote(key));
+  });
+};
