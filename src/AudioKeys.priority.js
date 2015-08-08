1bdf618bf0b17f8e7f22803f64cd610750c3b8a7
Kyle Stetz <kyle@punkave.com>
2015-08-08 13:53:08 -0400

    added note priority rules with passing tests!

--- src/AudioKeys.priority.js
+++ src/AudioKeys.priority.js
@@ -24,10 +24,8 @@ AudioKeys.prototype._prioritize = function() {
       key.isActive = false;
       return key;
     });
-    // set the last bunch to active based on the polyphony.
-    for(var i = self._state.keys.length - self._state.polyphony; i < self._state.keys.length; i++) {
-      self._state.keys[i].isActive = true;
-    }
+
+    self['_' + self._state.priority]();
   }
 
   // now take the isActive keys and set the new buffer.
@@ -40,4 +38,66 @@ AudioKeys.prototype._prioritize = function() {
   });
 
   // done.
+};
+
+AudioKeys.prototype._last = function() {
+  var self = this;
+  // set the last bunch to active based on the polyphony.
+  for(var i = self._state.keys.length - self._state.polyphony; i < self._state.keys.length; i++) {
+    self._state.keys[i].isActive = true;
+  }
+};
+
+AudioKeys.prototype._first = function() {
+  var self = this;
+  // set the last bunch to active based on the polyphony.
+  for(var i = 0; i < self._state.polyphony; i++) {
+    self._state.keys[i].isActive = true;
+  }
+};
+
+AudioKeys.prototype._highest = function() {
+  var self = this;
+  // get the highest notes and set them to active
+  var notes = self._state.keys.map( function(key) {
+    return key.note;
+  });
+
+  notes.sort( function(b,a) {
+    if(a === b) {
+      return 0;
+    }
+    return a < b ? -1 : 1;
+  });
+
+  notes.splice(self._state.polyphony, Number.MAX_VALUE);
+
+  self._state.keys.forEach( function(key) {
+    if(notes.indexOf(key.note) !== -1) {
+      key.isActive = true;
+    }
+  });
+};
+
+AudioKeys.prototype._lowest = function() {
+  var self = this;
+  // get the highest notes and set them to active
+  var notes = self._state.keys.map( function(key) {
+    return key.note;
+  });
+
+  notes.sort( function(a,b) {
+    if(a === b) {
+      return 0;
+    }
+    return a < b ? -1 : 1;
+  });
+
+  notes.splice(self._state.polyphony, Number.MAX_VALUE);
+
+  self._state.keys.forEach( function(key) {
+    if(notes.indexOf(key.note) !== -1) {
+      key.isActive = true;
+    }
+  });
 };
--- test/test.priority.js
+++ test/test.priority.js
@@ -8,6 +8,7 @@ describe('Priority', function() {
       var keyboard = new AudioKeys();
 
       keyboard.set('polyphony', 2);
+      keyboard.set('priority', 'last');
 
       keyboard._addKey({ keyCode: 65 });
       keyboard._addKey({ keyCode: 87 });
@@ -16,6 +17,127 @@ describe('Priority', function() {
       assert.equal(keyboard._state.buffer.length, 2);
       assert.equal(keyboard._state.buffer[0].keyCode, 87);
       assert.equal(keyboard._state.buffer[1].keyCode, 83);
+    });
+
+    it('should retrigger an inactive note when it becomes active again', function() {
+      var keyboard = new AudioKeys();
+
+      keyboard.set('polyphony', 1);
+      keyboard.set('priority', 'last');
+      keyboard._addKey({ keyCode: 65 });
+      keyboard._addKey({ keyCode: 87 });
+
+      var flag = false;
+      keyboard.up( function() {
+        flag = true;
+      });
+
+      keyboard._removeKey({ keyCode: 87 });
+      assert.equal(flag, true);
+    });
+  });
+
+  describe('`first`', function() {
+    it('should prioritize the first notes in the _keys array', function() {
+      var keyboard = new AudioKeys();
+
+      keyboard.set('polyphony', 2);
+      keyboard.set('priority', 'first');
+
+      keyboard._addKey({ keyCode: 65 });
+      keyboard._addKey({ keyCode: 87 });
+      keyboard._addKey({ keyCode: 83 });
+
+      assert.equal(keyboard._state.buffer.length, 2);
+      assert.equal(keyboard._state.buffer[0].keyCode, 65);
+      assert.equal(keyboard._state.buffer[1].keyCode, 87);
+    });
+
+    it('should retrigger an inactive note when it becomes active again', function() {
+      var keyboard = new AudioKeys();
+
+      keyboard.set('polyphony', 1);
+      keyboard.set('priority', 'first');
+      keyboard._addKey({ keyCode: 65 });
+      keyboard._addKey({ keyCode: 87 });
+
+      var flag = false;
+      keyboard.up( function() {
+        flag = true;
+      });
+
+      keyboard._removeKey({ keyCode: 65 });
+      assert.equal(flag, true);
+    });
+  });
+
+  describe('`highest`', function() {
+    it('should prioritize the highest notes in the _keys array', function() {
+      var keyboard = new AudioKeys();
+
+      keyboard.set('polyphony', 2);
+      keyboard.set('priority', 'highest');
+
+      keyboard._addKey({ keyCode: 79 }); // note = 73
+      keyboard._addKey({ keyCode: 84 }); // note = 66
+      keyboard._addKey({ keyCode: 89 }); // note = 68
+
+
+      assert.equal(keyboard._state.buffer.length, 2);
+      assert.equal(keyboard._state.buffer[0].keyCode, 79);
+      assert.equal(keyboard._state.buffer[1].keyCode, 89);
+    });
+
+    it('should retrigger an inactive note when it becomes active again', function() {
+      var keyboard = new AudioKeys();
+
+      keyboard.set('polyphony', 1);
+      keyboard.set('priority', 'highest');
+      keyboard._addKey({ keyCode: 65 });
+      keyboard._addKey({ keyCode: 87 });
+
+      var flag = false;
+      keyboard.up( function() {
+        flag = true;
+      });
+
+      keyboard._removeKey({ keyCode: 87 });
+      assert.equal(flag, true);
+    });
+  });
+
+  describe('`lowest`', function() {
+    it('should prioritize the lowest notes in the _keys array', function() {
+      var keyboard = new AudioKeys();
+
+      keyboard.set('polyphony', 2);
+      keyboard.set('priority', 'lowest');
+
+      keyboard._addKey({ keyCode: 84 });
+      keyboard._addKey({ keyCode: 79 });
+      keyboard._addKey({ keyCode: 89 });
+
+
+      assert.equal(keyboard._state.buffer.length, 2);
+      assert.equal(keyboard._state.buffer[0].keyCode, 84);
+      assert.equal(keyboard._state.buffer[1].keyCode, 89);
+    });
+
+    it('should retrigger an inactive note when it becomes active again', function() {
+      var keyboard = new AudioKeys();
+
+      keyboard.set('polyphony', 1);
+      keyboard.set('priority', 'lowest');
+      keyboard._addKey({ keyCode: 87 });
+      keyboard._addKey({ keyCode: 65 });
+
+      var flag = false;
+      keyboard.up( function() {
+        flag = true;
+      });
+
+      keyboard._removeKey({ keyCode: 65 });
+      assert.equal(flag, true);
     });
   });
 
