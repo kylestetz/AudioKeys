29754d32e28f5ee1746902bc6f723a3c87be2fa0
Kyle Stetz <kylestetz@gmail.com>
2017-11-18 09:03:46 -0800

    Update build to use browserify, add minified build

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,41 @@
 var gulp = require('gulp');
-var concat = require('gulp-concat');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
 var sourcemaps = require('gulp-sourcemaps');
+var minify = require('gulp-minify');
+var browserify = require('browserify');
 
 gulp.task('build', function() {
-  return gulp.src([
-      './src/AudioKeys.js',
-      './src/AudioKeys.state.js',
-      './src/AudioKeys.events.js',
-      './src/AudioKeys.mapping.js',
-      './src/AudioKeys.buffer.js',
-      './src/AudioKeys.priority.js',
-      './src/AudioKeys.special.js'
-    ])
-    .pipe(sourcemaps.init())
-    .pipe(concat('audiokeys.js'))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('dist'))
+  var b = browserify({
+    entries: './src/AudioKeys.js',
+    debug: false,
+    standalone: 'AudioKeys',
+  });
+
+  return b.bundle()
+    .pipe(source('audiokeys.js'))
+    .pipe(buffer())
+    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('minify', function() {
+  var b = browserify({
+    entries: './src/AudioKeys.js',
+    debug: false,
+    standalone: 'AudioKeys',
+  });
+
+  return b.bundle()
+    .pipe(source('audiokeys.js'))
+    .pipe(buffer())
+    .pipe(minify({
+        ext:{
+            src:'.js',
+            min:'.min.js'
+        },
+    }))
+    .pipe(gulp.dest('./dist/'));
   ;
 });
+
+gulp.task('default', ['build', 'minify']);
